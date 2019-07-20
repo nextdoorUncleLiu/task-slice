a1afbc0acfcc18aee8c75926dda7cdce53fbf2f8
刘希忱 <a260496725@qq.com>
2019-07-20 14:07:55 +0800

    初始化版本提交

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# time-slice
+## 时间切片
+
+这是一个用来性能优化的工具，如果项目中存在 `long task` 的任务，可以采用此工具来做性能优化
+
+### 核心思想
+
+用户在感知每秒 60 帧（也就是 16.7 毫秒）时候，动画是平滑过渡的
+
+在如果出现少于 60 帧动画的时候，就会感觉到卡顿，影响卡顿的原因有一部分就是因为 `long task` 导致的
+
+这个工具的目的，就是用来解决长任务
+
+### 安装
+
+```javascript
+npm install time-slice
+```
+
+### 使用方式
+
+```javascript
+import TimeSlice from 'TimeSlice'
+
+TimeSlice.init(number || array, function(i){
+    //i 执行到第几次，或者第几个切片任务
+})
+```
+
+### 亲测数据对比
+
+![](img/dataOne.png)
+![](img/dataTwo.png)
---
+++ index.js
@@ -0,0 +1,68 @@
+/**
+ * @name 时间切片
+ * @description 一个用来做性能优化的工具
+ * @author xichen Liu
+ * @version 1.0.0
+ */
+
+var raf = window.requestAnimationFrame ? window.requestAnimationFrame.bind(window) : setTimeout;
+
+var isNum = arg => arg instanceof Number && arg.constructor === Number;
+
+var isArray = arg => arg instanceof Array && arg.constructor === Array;
+
+var isFunction = arg => arg instanceof Function && arg.constructor === Function;
+
+function TimeSlice() {}
+
+TimeSlice.prototype = {
+	init: function ({ sliceList, callback }) {
+		if (!isFunction(callback)) {
+			console.error('callback 为必传参数并为 function');
+			return;
+		}
+		// 添加切片队列
+		this.generator = this.sliceQueue({
+			sliceList,
+			callback
+		});
+		// 开始切片
+		this.next();
+	},
+	/*
+	 * next [执行下次队列]
+	 * 依次执行切片，理想状态下每一个任务的执行时间不应该超过 16.7ms，如果超过了 16.7ms，就在启一个任务
+	 * 详情：https://developers.google.com/web/fundamentals/performance/rail
+	*/
+	next: function () {
+		const { generator } = this;
+		const start = performance.now();
+		let res = null;
+		do {
+			res = generator.next();
+		}
+		while (!res.done && performance.now() - start < 16.7);
+		if (res.done) return;
+		raf(this.next.bind(this));
+	},
+	/**
+	 * sliceQueue [切片]
+	 * @param { array | number } sliceList [切片数组或次数]
+	 * @param { function } callback [切片回调]
+	 */
+	sliceQueue: function* ({ sliceList, callback }) {
+		// 处理数组或次数
+		let listOrNum = (isNum(sliceList) && sliceList) || (isArray(sliceList) && sliceList.length);
+		for (let i = 0; i < listOrNum; ++i) {
+			const start = performance.now();
+			callback(i);
+			// 如果执行需要的时间少于 16.7ms，就停止继续执行下去
+			// 如果大于的话，就在下一次绘制的时候去执行
+			while (performance.now() - start < 16.7) {
+				yield;
+			}
+		}
+	}
+}
+
+export default new TimeSlice();
